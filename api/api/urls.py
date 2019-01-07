0f025a15b31a828a0c57b68141751cb9606a0063
Michael Reiswich <michael.reiswich@Michaels-MacBook-Pro.local>
2019-01-07 17:03:37 +0100

    +add login +base login frontend

--- api/api/urls.py
+++ api/api/urls.py
@@ -15,13 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import TemplateView
 
 from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('', views.index),
+    #path('', views.index),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('order', views.add_order),
     path('all_orders', views.get_all_orders),
     path('<int:id>', views.get_order),
